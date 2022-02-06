6a7bb0cb78cd622ba67d63f39e2970d120be0a36
Ubuntu <ubuntu@ip-172-31-3-11.ec2.internal>
2022-02-06 01:55:38 +0000

    updating files

--- cloud_resources/listener.py
+++ cloud_resources/listener.py
@@ -12,22 +12,24 @@ LOCAL_MQTT_PORT=1883
 LOCAL_MQTT_TOPIC="mypub"
 
 # S3 connection
-s3 = boto3.resource('s3')
+#s3 = boto3.resource('s3')
 
 #data = open('Nvidia_Cloud_EULA.pdf', 'rb')
 #s3.Bucket('nphw3').put_object(Key='Nvidia_Cloud_EULA.pdf', Body=data)
 
-#count = 0
-#def save_img(img_bytes):
-#    global count
-#    response = s3client.put_object( 
-#    Bucket='nphw3',
-#    Body=img_bytes,
+s3client = boto3.client('s3')
 
-#Key='face{:d}.png'.format(count),
-#    ACL='public-read',
-#    ContentType='image/png'
+count = 0
+def save_img(img_bytes):
+    global count
+    response = s3client.put_object( 
+    Bucket='nphw3',
+    Body=img_bytes,
+    Key='face{:d}.png'.format(count),
+    ACL='public-read',
+    ContentType='image/png'
   )
+    count+=1
 
 def on_connect_local(client, userdata, flags, rc):
     print("connected to local broker with rc: " + str(rc))
@@ -37,15 +39,11 @@ def on_connect_local(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     try:
         # File Name
-        date_t = datetime.now().strftime('%m-%d-%Y-%H-%M-%S')
-        image_name = str(date_t) + '_image.png'
-
+        #date_t = datetime.now().strftime('%m-%d-%Y-%H-%M-%S')
+        #image_name = str(date_t) + '_image.png'
         # Save image
-        msd = np.frombuffer(msg, np.uint8)
-        msg = cv2.imdecode(msd, cv2.IMREAD_COLOR)
-        cv2.imwrite(image_name, msg)
-        data = msg
-        s3.Bucket('nphw3').put_object(Key='image_name', Body=data)
+        msg = msg.payload
+        save_img(msg)
 
     except:
         print("Unexpected error:", sys.exc_info()[0])
